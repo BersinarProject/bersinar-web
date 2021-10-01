c8ade4069746cc95cd016fb0d6fd3a7d5b6d50f0
Muhammad Arief Fadhlan <ariffadhlan12@gmail.com>
2021-10-01 19:02:24 +0700

    add axios and update upcoming class card

--- bersinar-web/src/components/upcoming-class-card/upcomingClassCard.js
+++ bersinar-web/src/components/upcoming-class-card/upcomingClassCard.js
@@ -5,62 +5,54 @@ import { Card } from "react-bootstrap";
 import "./upcomingClassCard.css";
 import cardImage1 from "../../assets/images/starwars.jpg";
 
-const card = (props) => {
+const upcomingClasscard = ({ dataCard }) => {
 	return (
-		<div className="cards-class container d-flex flex-wrap justify-content-evenly align-items-center">
-			{props.dataUpcomingClassCard.map((dataCard) => {
-				return (
-					<Card
-						key={dataCard.id}
-						className="card-class border-0 shadow mb-5"
-						style={{ width: "350px" }}
-					>
-						<Card.Img
-							variant="top"
+		<Card
+			key={dataCard.id}
+			className="card-class border-0 shadow mb-5"
+			style={{ width: "350px" }}
+		>
+			<Card.Img
+				variant="top"
+				src={cardImage1}
+				className="card-class-image"
+			/>
+			<Card.Body style={{ padding: "28px" }}>
+				<div className="card-class-category d-flex justify-content-between">
+					<Card.Text className="card-class-category-name">
+						{dataCard.courseName}
+					</Card.Text>
+					<Card.Text className="card-class-category-tagline">
+						{dataCard.courseCode}
+					</Card.Text>
+				</div>
+				<Card.Title className="card-class-title fs-6 lh-base fw-bold">
+					{dataCard.subjectName}
+				</Card.Title>
+				<Card.Text className="card-class-text">
+					{dataCard.subjectDescription}
+				</Card.Text>
+				<div className="card-class-tutors d-flex justify-content-between align-items-center">
+					<div className="card-class-tutor d-flex align-items-center">
+						{/* <img
 							src={cardImage1}
-							className="card-class-image"
-						/>
-						<Card.Body style={{ padding: "28px" }}>
-							<div className="card-class-category d-flex justify-content-between">
-								<Card.Text className="card-class-category-name">
-									{dataCard.category}
-								</Card.Text>
-								<Card.Text className="card-class-category-tagline">
-									{dataCard.tagLine}
-								</Card.Text>
-							</div>
-							<Card.Title className="card-class-title fs-6 lh-base fw-bold">
-								{dataCard.title}
-							</Card.Title>
-							<Card.Text className="card-class-text">
-								Lorem ipsum dolor sit amet, consectetur
-								adipiscing elit, sed do eiusmod tempor
-								incididunt ut labore et dolore magna aliqua.
-							</Card.Text>
-							<div className="card-class-tutors d-flex justify-content-between align-items-center">
-								<div className="card-class-tutor d-flex align-items-center">
-									<img
-										src={cardImage1}
-										className="card-class-tutor-image"
-										alt="Tutor"
-									/>
-									<Card.Text className="card-class-tutor-name">
-										{dataCard.tutor}
-									</Card.Text>
-								</div>
-								<Link
-									to="/our-class"
-									className="card-class-daftar text-decoration-none"
-								>
-									Join Kelas
-								</Link>
-							</div>
-						</Card.Body>
-					</Card>
-				);
-			})}
-		</div>
+							className="card-class-tutor-image"
+							alt="Tutor"
+						/> */}
+						<Card.Text className="card-class-tutor-name">
+							{dataCard.tutorName}
+						</Card.Text>
+					</div>
+					<Link
+						to="/our-class"
+						className="card-class-daftar text-decoration-none"
+					>
+						Join Kelas
+					</Link>
+				</div>
+			</Card.Body>
+		</Card>
 	);
 };
 
-export default card;
+export default upcomingClasscard;
--- bersinar-web/src/containers/upcoming-class/UpcomingClass.js
+++ bersinar-web/src/containers/upcoming-class/UpcomingClass.js
@@ -1,50 +1,65 @@
-import { React, useState } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
-import Card from "../../components/upcoming-class-card/upcomingClassCard";
+import UpcomingClassCard from "../../components/upcoming-class-card/upcomingClassCard";
 import "./UpcomingClass.css";
 
-const UpcomingClass = () => {
-	const [getDataUpcomingClassCard] = useState([
-		{
-			id: 1,
-			category: "Saintek",
-			tagLine: "#MenyinariFisika",
-			title: "Belajar Fisika Kuantum Bareng Razan",
-			tutor: "Razan"
-		},
-		{
-			id: 2,
-			category: "Soshum",
-			tagLine: "#MenyinariEkonomi",
-			title: "Belajar Dasar Ekonomi Bareng Yasmin",
-			tutor: "Yasmin"
-		},
-		{
-			id: 3,
-			category: "TPS",
-			tagLine: "#MenyinariEnglish",
-			title: "Tenses and Conjunction with Ahnaf",
-			tutor: "Ahnaf"
-		}
-	]);
+class upcomingClass extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			dataCards: []
+		};
+	}
 
-	return (
-		<div className="upcoming-class">
-			<div className="upcoming-class-header d-flex flex-column justify-content-center align-items-center">
-				<h1 className="upcoming-class-title mb-3">Upcoming Classes</h1>
-				<h2 className="upcoming-class-subTitle">
-					Kelas-kelas Bersinar yang akan diadakan selanjutnya
-				</h2>
-			</div>
-			<Card dataUpcomingClassCard={getDataUpcomingClassCard} />
-			<div className="btn d-flex justify-content-center align-items-center">
-				<Link to="/our-class" className="btn-more btn btn-shadow px-4">
-					More Class
-				</Link>
-			</div>
-		</div>
-	);
-};
+	componentDidMount() {
+		const apiUrl = "https://api.good4u.tech/classes?type=3";
 
-export default UpcomingClass;
+		axios
+			.get(apiUrl)
+			.then((response) => {
+				this.setState({
+					dataCards: response.data.classes
+				});
+			})
+			.catch((error) => {
+				return error;
+			});
+	}
+
+	renderItems() {
+		const { dataCards } = this.state;
+		return dataCards.map((dataCard) => {
+			return <UpcomingClassCard key={dataCard.id} dataCard={dataCard} />;
+		});
+	}
+
+	render() {
+		return (
+			<div className="upcoming-class">
+				<div className="upcoming-class-header d-flex flex-column justify-content-center align-items-center">
+					<h1 className="upcoming-class-title mb-3">
+						Upcoming Classes
+					</h1>
+					<h2 className="upcoming-class-subTitle">
+						Kelas-kelas Bersinar yang akan diadakan selanjutnya
+					</h2>
+				</div>
+				<div className="cards-class container d-flex flex-wrap justify-content-evenly align-items-center">
+					{this.renderItems()}
+				</div>
+				<div className="btn d-flex justify-content-center align-items-center">
+					<Link
+						to="/our-class"
+						className="btn-more btn btn-shadow px-4"
+					>
+						More Class
+					</Link>
+				</div>
+			</div>
+		);
+	}
+}
+
+export default upcomingClass;
