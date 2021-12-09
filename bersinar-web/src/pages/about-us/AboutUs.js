1ebe9420044a24d083e4350cbaef728fea65f4f2
razanfawwaz <razanfawwaz1905@gmail.com>
2021-12-10 02:29:26 +0700

    core values

--- bersinar-web/src/pages/about-us/AboutUs.js
+++ bersinar-web/src/pages/about-us/AboutUs.js
@@ -4,11 +4,11 @@ import { Container } from "react-bootstrap";
 
 import "./AboutUs.css";
 import Navbar from "../../components/navbar/Navbar";
+import Footer from "../../components/footer/footer";
 
-import Footer from "../../components/footer/Footer";
 import aboutUsHero from "../../assets/images/about-us/aboutUsHero.svg";
 import bersinarLogoIcon from "../../assets/images/bersinar-logo/bersinarLogoIcon.svg";
-import presidenBersinar from "../../assets/images/about-us/presidenBersinar.png";
+import presidenBersinar from "../../assets/images/about-us/PresidenBersinar.png";
 import misi1 from "../../assets/images/about-us/misi/misi-1.svg";
 import misi2 from "../../assets/images/about-us/misi/misi-2.svg";
 import misi3 from "../../assets/images/about-us/misi/misi-3.svg";
@@ -16,7 +16,6 @@ import misi4 from "../../assets/images/about-us/misi/misi-4.svg";
 import achievement from "../../assets/images/about-us/achievement.svg";
 import FAQIllustration from "../../assets/images/FAQIllustration.png";
 import FAQButtonIcon from "../../assets/images/FAQButtonIcon.svg";
-import bersinarLogo from "../../assets/images/about-us/BersinarLogo.png";
 import coreIcon from "../../assets/images/coreIcon.svg";
 
 export default function AboutUs() {
